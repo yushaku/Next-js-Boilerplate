90ed818012a66efcbda8ff675121c92ac16a3e7d
Yushaku <levanson180200@gmail.com>
2023-09-22 23:54:26 +0700

    add prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true,
+  endOfLine: 'auto',
+  trailingComma: 'none',
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  importOrder: ['^react$', '^components/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports']
+}
