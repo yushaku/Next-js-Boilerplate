6e1d086ddcc8cad13f1ca4c1840ed92ca4501428
Yushaku <levanson180200@gmail.com>
2023-09-23 20:57:06 +0700

    css border animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-'use client'
-
-import { Button } from '@material-tailwind/react'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>adjasdlfhs</Button>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-[#0e1538] p-24">
+      <button className="gradient-border">
+        <span className="gradient-inner">click here</span>
+      </button>
+
+      <button className="card">
+        <span className="card-content">card</span>
+      </button>
     </main>
   )
 }
